9aa203dbf1b7e2e146d7a96807f2771b3b1d90ef
Trishna Chakraborty <trishnachakraborty13@gmail.com>
2019-06-11 15:22:50 +0600

    dead queue is working fully

--- src/main/java/com/example/rabbitmqspringpoc/Sender.java
+++ src/main/java/com/example/rabbitmqspringpoc/Sender.java
@@ -2,13 +2,20 @@ package com.example.rabbitmqspringpoc;
 
 
 import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageBuilder;
+import org.springframework.amqp.core.MessageDeliveryMode;
 import org.springframework.amqp.core.MessageProperties;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 public class Sender {
@@ -28,12 +35,21 @@ public class Sender {
         //rabbitTemplate.convertAndSend(RabbitmqspringpocApplication.topicExchangeName, "foo.bar.baz", "Hello from RabbitMQ!");
         System.out.println(" [x] Requesting fib(" + no + ")");
 
-        MessageProperties props = new MessageProperties();
-        props.setReplyTo("reply-queue");
+    /*    MessageProperties props = new MessageProperties();
+        props.setReplyTo("reply");*/
 
-        Message toSend = new Message(no.toString().getBytes(), props);
-        Integer response= (Integer)rabbitTemplate.convertSendAndReceive("customer_exchange", "",toSend);
-        System.out.println(" [.] Got '" + response + "'");
+
+
+        //Message toSend = new Message(no.toString().getBytes(), props);
+        rabbitTemplate.convertAndSend("customer_exchange", "",no);
+        //System.out.println(" [.] Got '" + response + "'");
+    }
+
+
+    @RabbitListener(queues = "reply_queue")
+    public void fibonacci(int n) throws IOException {
+        System.out.println(" [.] Got '" +n+ " in reply");
+
     }
 
 
--- src/main/java/com/example/rabbitmqspringpoc/Server.java
+++ src/main/java/com/example/rabbitmqspringpoc/Server.java
@@ -1,19 +1,26 @@
 package com.example.rabbitmqspringpoc;
 
+import com.rabbitmq.client.Channel;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.SendTo;
+
+import java.io.IOException;
 
 public class Server {
 
 
     //https://www.javacodegeeks.com/2018/02/springboot-messaging-rabbitmq.html
     //sent message as json
-   /* @RabbitListener(queues = "customer")
-    @SendTo("reply_queue")*/
-    public int fibonacci(int n) {
+   // @RabbitListener(queues = "customer")
+    //@SendTo("reply")
+    public int fibonacci(int n, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long tag) throws IOException {
         System.out.println(" [x] Received request for " + n);
+        if(n>=10){channel.basicAck(tag,false); return 0;}
         int result = fib(n);
         System.out.println(" [.] Returned " + result);
+        channel.basicAck(tag,false);
         return result;
     }
 
@@ -23,10 +30,13 @@ public class Server {
 
     @RabbitListener(queues = "dead_queue")
     @SendTo("reply_queue")
-    public int deadfibonacci(int n) {
+    public int deadfibonacci(int n , Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long tag) throws IOException {
         System.out.println(" [x] Received request for dead " + n);
+        if(n>=10){channel.basicAck(tag,false); return 0;}
         int result = fib(n);
+
         System.out.println(" [.] Returned dead " + result);
+        channel.basicAck(tag,false);
         return result;
     }
 
